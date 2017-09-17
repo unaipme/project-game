173c7771610a18d701653decf855dd62a11e95a8
Unai P. Mendizabal <unaipme@gmail.com>
2017-09-17 21:57:28 +0200

    Añado ranking funcional, queda pendiente del diseño definitivo

--- src/main/java/com/retrolaza/game/screens/RankingScreen.java
+++ src/main/java/com/retrolaza/game/screens/RankingScreen.java
@@ -4,18 +4,35 @@ import java.awt.FontFormatException;
 import java.awt.Graphics2D;
 import java.awt.event.KeyEvent;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.ListIterator;
+import java.util.Map;
+import java.util.Scanner;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.HttpClientBuilder;
+
+import com.jayway.jsonpath.JsonPath;
 import com.retrolaza.game.Game;
 import com.retrolaza.game.GameScreen;
 import com.retrolaza.game.controls.KeyboardControls;
 import com.retrolaza.game.drawable.Text;
+import com.retrolaza.game.screens.data.Record;
+
+import net.minidev.json.JSONArray;
 
 public class RankingScreen extends GameScreen {
 	
 	private KeyboardControls controls;
 	private Text titleText;
+	private Map<Integer, Text> ranking;
+	private String loadingError;
 	
 	public static final int DR_TITLE = Game.ID.getAndIncrement();
+	
+	private static final String RANKING_GENERAL_URL = "https://r9ovtf8cli.execute-api.eu-west-1.amazonaws.com/alpha/ranking";
 
 	public RankingScreen(Game game, MainScreen mainScreen) throws FontFormatException, IOException {
 		super(game, mainScreen);
@@ -28,6 +45,8 @@ public class RankingScreen extends GameScreen {
 		
 		titleText = new Text("RANKING", 30, 30);
 		addDrawable(DR_TITLE, titleText);
+		
+		loadRanking();
 		
 		this.hiding = true;
 	}
@@ -45,6 +64,35 @@ public class RankingScreen extends GameScreen {
 	@Override
 	public void draw(Graphics2D g2d) {
 		super.draw(g2d);
+	}
+	
+	@SuppressWarnings("unchecked")
+	private void loadRanking() throws FontFormatException {
+		ranking = new HashMap<>();
+		
+		HttpClient http = HttpClientBuilder.create().build();
+		HttpGet request = new HttpGet(RANKING_GENERAL_URL);
+		try {
+			HttpResponse response = http.execute(request);
+			Scanner scanner = new Scanner(response.getEntity().getContent(), "UTF-8");
+			String json = scanner.useDelimiter("\\A").next();
+			scanner.close();
+			JSONArray array = JsonPath.read(json, "$[*]");
+			ListIterator<Object> objectList = array.listIterator();
+			int i = 0;
+			while (objectList.hasNext()) {
+				Map<String, Object> info = (Map<String, Object>) objectList.next();
+				Record r = new Record().withPosition((Integer) info.get("position")).withScore((Integer) info.get("score")).withUsername(info.get("username").toString());
+				Text t = new Text(String.format("%s %d", r.getUsername(), r.getScore()), 30, 60 + i * 30);
+				int id = Game.ID.getAndIncrement();
+				ranking.put(id, t);
+				addDrawable(id, t);
+				i++;
+			}
+		} catch (IOException e) {
+			loadingError = "Akats bat gertatu da";
+			e.printStackTrace();
+		}
 	}
 
 }
---
+++ src/main/java/com/retrolaza/game/screens/data/Record.java
@@ -0,0 +1,47 @@
+package com.retrolaza.game.screens.data;
+
+public class Record {
+	private String username;
+	private Integer position;
+	private Integer score;
+	
+	public Record withUsername(String username) {
+		this.username = username;
+		return this;
+	}
+	
+	public Record withPosition(Integer position) {
+		this.position = position;
+		return this;
+	}
+	
+	public Record withScore(Integer score) {
+		this.score = score;
+		return this;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public Integer getPosition() {
+		return position;
+	}
+
+	public void setPosition(Integer position) {
+		this.position = position;
+	}
+
+	public Integer getScore() {
+		return score;
+	}
+
+	public void setScore(Integer score) {
+		this.score = score;
+	}
+	
+}
